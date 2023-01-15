3d5ce1517682f3717116e069b202ac3cf6b55a3e
NEXTAltair <NEXTAltair@users.noreply.github.com>
2023-01-15 14:42:48 +0900

    放送大学ラジオ.tsがおかしいのでMP4にエンコする処理を追加｡それでもおかしい

--- movie2voice.py
+++ movie2voice.py
@@ -1,4 +1,6 @@
 #音声だけを取り出す
+#tsはMP4へエンコしてから変換
+#tsを直にwavにすると変換はできているがエラーを吐いて処理が止まる
 import os
 import ffmpeg
 
@@ -16,6 +18,11 @@ for root, dirs, files in os.walk(src_folder):
     for file in files:
         if file.endswith(ext):
             input_file = os.path.join(root, file)
-            output_file = os.path.join(dst_folder, os.path.splitext(file)[0]+".wav")
+            #tsは先にmp4へ変換処理
+            if file.endswith('.ts'):
+                temp_file = os.path.join(root, os.path.splitext(file)[0]+".mp4")
+                ffmpeg.input(input_file).output(temp_file).run()
+                input_file = temp_file
             # ffmpegを使用して音声のみを抽出し、変換
+            output_file = os.path.join(dst_folder, os.path.splitext(file)[0]+".wav")
             ffmpeg.input(input_file).output(output_file, acodec='pcm_s16le', vcodec='none').run()
