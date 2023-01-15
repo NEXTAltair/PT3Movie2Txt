ed938a943dc3896f3c5b30c06f4b7c24d12dc438
NEXTAltair <NEXTAltair@users.noreply.github.com>
2023-01-15 12:06:38 +0900

    フォルダ内のファイルから音声抜き出しまではできた

--- movie2voice.py
+++ movie2voice.py
@@ -1,16 +1,20 @@
+#音声だけを取り出す
 import os
 import ffmpeg
-# 変換するファイルパス
-folder = "./TestFile"
 
-# 変換したいファイル (e.g. mp3, flac, etc.)
-ext = ".ts"
+# 変換元のフォルダ
+src_folder = "TestFile"
 
-for filename in os.listdir(folder):
-    if filename.endswith(ext):
-        input_file = os.path.join(folder, filename)
-        output_file = os.path.join(folder, os.path.splitext(filename)[0]+".wav")
-        # use ffmpeg-python to convert the file
-        stream = ffmpeg.input(input_file)
-        stream = ffmpeg.output(stream, output_file)
-        ffmpeg.run(stream)
+# 変換先のフォルダ
+dst_folder = "wav"
+
+# 変換したいファイルの拡張子
+ext = ('.m4v', '.ts', '.mp3', '.mp4')
+
+# 変換元のフォルダ内のファイルを検索
+for file in os.scandir(src_folder):
+    if file.is_file() and file.name.endswith(ext):
+        input_file = file.path
+        output_file = os.path.join(dst_folder, os.path.splitext(file.name)[0]+".wav")
+        # ffmpegを使用して音声のみを抽出し、変換
+        ffmpeg.input(input_file).output(output_file, acodec='pcm_s16le', vcodec='none').run()
