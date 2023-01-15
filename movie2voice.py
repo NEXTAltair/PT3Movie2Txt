6b788f9971c17a6b59ea426e92bc3d195a0b3ea3
NEXTAltair <NEXTAltair@users.noreply.github.com>
2023-01-15 18:05:57 +0900

    音を抜き出すまではOK

--- movie2voice.py
+++ movie2voice.py
@@ -1,11 +1,13 @@
 #音声だけを取り出す
-#tsはMP4へエンコしてから変換
+#tsはmp4にエンコしてから変換
 #tsを直にwavにすると変換はできているがエラーを吐いて処理が止まる
+#ゴミ箱にあるファイルを参照してエラーで止まる
+#temp_fileが重複してると上書きするか聞かれるのですスキップ
 import os
 import ffmpeg
 
 # 変換元のフォルダ
-src_folder = "TestFile"
+src_folder = "L:"
 
 # 変換先のフォルダ
 dst_folder = "wav"
@@ -15,14 +17,25 @@ ext = ('.m4v', '.ts', '.mp3', '.mp4')
 
 # 変換元のフォルダ内のファイルを検索
 for root, dirs, files in os.walk(src_folder):
+    #ゴミ箱にあるファイルをスキップする処理
+    if '$RECYCLE.BIN' in root:
+        continue
     for file in files:
         if file.endswith(ext):
             input_file = os.path.join(root, file)
             #tsは先にmp4へ変換処理
             if file.endswith('.ts'):
                 temp_file = os.path.join(root, os.path.splitext(file)[0]+".mp4")
+                # 同名の.mp4が存在する場合はスキップ
+                if os.path.exists(output_file):
+                    print(output_file + " already exists.")
+                    continue
                 ffmpeg.input(input_file).output(temp_file).run()
                 input_file = temp_file
-            # ffmpegを使用して音声のみを抽出し、変換
+            # ffmpegを使用して音声のみを抽出しwavへ変換
             output_file = os.path.join(dst_folder, os.path.splitext(file)[0]+".wav")
+            # 同名の.wavが存在する場合はスキップ
+            if os.path.exists(output_file):
+                print(output_file + " already exists.")
+                continue
             ffmpeg.input(input_file).output(output_file, acodec='pcm_s16le', vcodec='none').run()
