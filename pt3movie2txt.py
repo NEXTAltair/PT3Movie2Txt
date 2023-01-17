e2f61f7ff6d31fdd39b8e8b472fe846e0b94950e
NEXTAltair <NEXTAltair@users.noreply.github.com>
2023-01-17 12:04:24 +0900

    とりあえずやりたいことはできた

--- pt3movie2txt.py
+++ pt3movie2txt.py
@@ -1,7 +1,8 @@
-#wavをwhisperでTXTにするやつ
+#wavをwhisperでtxtにするやつ
 import os
 import glob
 import whisper
+import json
 
 #モデルロード最新版 #GPT3いわくaudio_path前にしないと処理が遅くならしい
 model = whisper.load_model("large-v2")
@@ -17,12 +18,14 @@ audio_path = glob.glob(os.path.join(wav_folder, "*.wav"))
 
 #model.transcribeはリストでファイルをは受け付けないのでforが必要
 for audio_file in audio_path:
+    #文字起こし
+    result = model.transcribe(audio=audio_file, verbose=True, language="ja")
     # 出力処理ファイルの準備
     output_file = os.path.join(output_folder, os.path.basename(audio_file).replace(".wav", ".txt"))
-    #文字起こしでverbose=True時間ごとの進捗をターミナルに表示
-    result = model.transcribe(audio=audio_file, verbose=True, language="ja")
-    #, encoding='utf-8'と, ensure_ascii=False忘れると文字コードしか出ない
-    with open(output_file, "w", encoding='utf-8') as f:    
-        f.write(result["text"])
-        #txtに保存された内容をコンソールに表示
-        print(result["text"])
+    #resultはdict(辞書)型なのでjsonモジュールのjson.dumpでstr(文字列)型に変換
+    transcribed_text = json.dumps(result)
+    #, encoding='utf-8'忘れると文字コードしか出ない
+    with open(output_file, "w", encoding='utf-8') as f:
+        #result内ではsegmentsで分割されてるのでその中からidとtextを取り出して一行ずつtxtに保存
+        for segment in result["segments"]:
+            f.write(f"id:{segment['id']} {segment['text']}\n")
